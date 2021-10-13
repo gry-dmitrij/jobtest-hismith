f8bc90c7e8c690d69e45464e32f626b8f9a9dbb6
Dmitriy Gryaznykh <gry-dmitrij@yandex.ru>
2021-10-13 14:55:05 +0300

    Центрует название приложения по центру

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,24 @@
       app
       color="primary"
       dark
-      class="d-flex"
     >
-      <v-btn v-show="$route.path !== '/'"
-             icon
-             class="mr-auto"
-             @click="$router.push('/')">
-        <v-icon>mdi-arrow-left-thick</v-icon>
-      </v-btn>
-      <h1 class="mr-auto ml-auto">Лента новостей</h1>
+      <v-container>
+        <v-row>
+          <v-col cols="1">
+            <v-btn v-show="$route.path !== '/'"
+                   icon
+                   class="mr-auto"
+                   @click="$router.push('/')">
+              <v-icon>mdi-arrow-left-thick</v-icon>
+            </v-btn>
+          </v-col>
+          <v-col cols="10"
+                 class="d-flex justify-center align-center">
+            <h2 class="mr-auto ml-auto">Лента новостей</h2>
+          </v-col>
+        </v-row>
+      </v-container>
+
     </v-app-bar>
     <v-main>
       <router-view/>
